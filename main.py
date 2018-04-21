0cbfcebe9b664ebca1fa0a45e22967bf41ebb9a3
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2018-04-20 19:09:55 -0600

    Inicio de Menu

--- main.py
+++ main.py
@@ -8,3 +8,43 @@
 # diferentes funciones basicas para ser aplicado a un Club de Video y poder llevar un control sobre 
 # las peliculas y los usuarios.
 
+#!/usr/bin/env python
+
+# Seccion de Importe de Archivos y Clases
+
+
+
+# Clases
+def main():
+
+	# Menu inicial
+
+	# Seccion principal de programa de python
+	menu()
+
+def menu():
+	# llamar a esta funcion
+
+	# mostras las opciones de menu
+	opciones_de_menu()
+
+	# preguntar que opcion quiere hacer, se debe evaluar que funcion es
+		
+def saludo_inicial():
+	print("* --------------------------------------- *")
+	print("|                                         |")
+	print("|           Inicializando Sistema         |")
+	print("|                                         |")
+	print("|              Cargando Datos             |")
+	print("|                                         |")
+	print("* --------------------------------------- *")
+	print("Inicializando sistema, por favor espere...")
+
+
+def opciones_de_menu():
+	print("Por favor ingrese una opcion:")
+	print("1. BlaBla")
+	print("2. BlaBla")
+	print("3. BlaBla")
+	print("4. BlaBla")
+	print("5. BlaBla")
